a3056c2bb247e0fa484abb65b8e7c84b2acbbaff
ndprime <nikechideradivine@gmail.com>
2025-09-07 21:35:16 +0100

    all done

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import About from './pages/About'
-import Contact from './pages/Contact'
+import Contact from './components/Contact'
 import Home from './pages/Home'
 import Services from './pages/Services'
 
