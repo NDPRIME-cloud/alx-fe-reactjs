636afaa9191001e121e69d3699b62db31408b648
ndprime <nikechideradivine@gmail.com>
2025-09-28 01:04:25 +0100

    task three

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg  " >
-      <img src="https://via.placeholder.com/150" alt="User" className="w-36 h-36 rounded-full mx-auto" />
-      <h1 className="text-xl text-blue-800 my-4 ">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 p-8-md p-4-sm max-w-xm max-w-sm mx-auto my-20 rounded-lg shadow-lg  " >
+      <img src="https://via.placeholder.com/150" alt="User" className="w-36-md h-36-md rounded-full mx-auto w-24-sm h-24-sm" />
+      <h1 className="text-xl-md text-lg-sm text-blue-800 my-4 ">John Doe</h1>
+      <p className="text-gray-600 text-base-md text-sm-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
