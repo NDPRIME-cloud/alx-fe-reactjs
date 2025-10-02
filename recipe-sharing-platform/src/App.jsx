2c11f57b6fccd424f1f841737c04491d1e151b3e
ndprime <nikechideradivine@gmail.com>
2025-10-02 11:48:36 +0100

    task 1

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Letters from './components/Letters'
+
+function App() {
+ 
+  return (
+    <>
+  
+  <Letters/>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ recipe-sharing-platform/src/components/Letters.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Letters = () => {
+  return (
+    <div>
+        <p className='text-3xl font-bold underline'>you are welcome</p>
+    </div>
+  )
+}
+
+export default Letters
