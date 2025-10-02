78403a15860d48b57d11388dc22f220d3709d1a7
ndprime <nikechideradivine@gmail.com>
2025-10-02 12:26:58 +0100

    task 2

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8 ">
+    <div className="container mx-auto px-4 py-8  grid-cols-1">
       <h1 className="text-3xl font-bold text-center mb-8"> Recipe Sharing Platform</h1>
 
       {/* Responsive Grid */}
